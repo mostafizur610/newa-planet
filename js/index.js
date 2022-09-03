ab5d38faf5e1e0004f6c844bbaeab3d62aaf3067
mostafizur610 <mostafizurrahman273@gmail.com>
2022-09-03 10:24:33 +0600

    categori added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
-// console.log('news planet')
+
+const loadCategories = () => {
+    fetch('https://openapi.programming-hero.com/api/news/categories')
+        .then(res => res.json())
+        .then(data => displayCategories(data.data.news_category))
+}
+const displayCategories = categories => {
+
+
+    const categoriesContainer = document.getElementById('categories-container');
+    categories.forEach(category => {
+        console.log(category)
+        const categoryDiv = document.createElement('div');
+        categoryDiv.classList.add('category');
+        categoryDiv.innerHTML = `
+
+<h6>${category.category_name}</h6>
+
+        `;
+        categoriesContainer.appendChild(categoryDiv);
+    })
+}
+
+loadCategories();
