e0e2ec66530266148717928391ad45fa6c263b96
mostafizur610 <mostafizurrahman273@gmail.com>
2022-09-04 10:16:06 +0600

    length and error handler added

--- js/index.js
+++ js/index.js
@@ -1,8 +1,14 @@
 
-const loadCategories = () => {
-    fetch('https://openapi.programming-hero.com/api/news/categories')
-        .then(res => res.json())
-        .then(data => displayCategories(data.data.news_category))
+const loadCategories = async () => {
+    try {
+        const res = await fetch('https://openapi.programming-hero.com/api/news/categories');
+        const data = await res.json();
+        // console.log(data);
+        displayCategories(data.data.news_category);
+    } catch (error) {
+        console.log('error', error);
+    }
+
 }
 
 
@@ -23,16 +29,23 @@ const loadCategoryDetails = async (category_id) => {
     const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`;
     const res = await fetch(url);
     const data = await res.json();
-    await displayCategorieDetails(data.data);
+    const sortData = data.data.sort((a, b) => b.total_view - a.total_view);
+    await displayCategorieDetails(sortData);
     // console.log('get details', category_id);
 }
 
 
 const displayCategorieDetails = async categoryData => {
     // console.log('categoryData', categoryData);
-
+    const totalCat = categoryData.length === 0 ? 'No News Found' : `${categoryData.length}  items found`;
+    // console.log('totalCat', totalCat);
+    const totalLength = document.getElementById('total-length');
+    totalLength.innerHTML = `
+    <p class="text-center p-4">${totalCat}</p>
+`;
     const categoryDetails = document.getElementById('category-details');
     categoryDetails.innerHTML = '';
+
     categoryData.forEach(cat => {
         // console.log('cat', cat)
         const catDiv = document.createElement('div');
@@ -46,8 +59,8 @@ const displayCategorieDetails = async categoryData => {
            </div>
         <div class="col-md-8">
             <div class="card-body">
-               <h5 class="card-title">"${cat.title}" </h5>
-               <p class="card-text">"${cat.details}"</p>
+               <h5 class="card-title">${cat.title}</h5>
+               <p class="card-text">${cat.details.length > 200 ? cat.details.slice(0, 200) + '...' : 'cat.details'}</p>
 
         <div class="d-flex justify-content-between">             
                <div class="d-flex justify-content-between">
@@ -59,7 +72,7 @@ const displayCategorieDetails = async categoryData => {
 
 </div>
             
-         <p class="card-text">${cat.total_view} M</p>
+         <p class="card-text">${cat.total_view} Views</p>
         <button onclick="loadnNewsDetails('${cat._id}')" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Show Details</button>
         </div>
         <div>
@@ -102,3 +115,8 @@ const dispayLoadNewsDetails = news => {
 }
 
 loadCategories();
+
+
+
+
+
